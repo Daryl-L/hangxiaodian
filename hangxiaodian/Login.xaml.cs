d12e14908d2a85099823aa28a021b80e25e6137d
Daryl <sai9375@hotmail.com>
2015-06-10 22:30:26 +0800

    做好了登录页面

--- hangxiaodian/Login.xaml.cs
+++ hangxiaodian/Login.xaml.cs
@@ -5,7 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI.Popups;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -14,20 +15,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=391641 上有介绍
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkID=390556 上有介绍
 
 namespace hangxiaodian
 {
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Login : Page
     {
-        public MainPage()
+        public Login()
         {
             this.InitializeComponent();
-
-            this.NavigationCacheMode = NavigationCacheMode.Required;
         }
 
         /// <summary>
@@ -37,13 +36,8 @@ namespace hangxiaodian
         /// 此参数通常用于配置页。</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            // TODO: 准备此处显示的页面。
-
-            // TODO: 如果您的应用程序包含多个页面，请确保
-            // 通过注册以下事件来处理硬件“后退”按钮:
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed 事件。
-            // 如果使用由某些模板提供的 NavigationHelper，
-            // 则系统会为您处理该事件。
+            var statusBar = StatusBar.GetForCurrentView();
+            statusBar.ForegroundColor = Color.FromArgb(0, 0, 0, 0);
         }
     }
 }
