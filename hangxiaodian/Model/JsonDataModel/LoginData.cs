396e3ee9269ed2cf623b92b6f685a9e5b6e6108a
Daryl <sai9375@hotmail.com>
2015-06-13 00:28:03 +0800

    添加了登录返回json的数据结构

---
+++ hangxiaodian/Model/JsonDataModel/LoginData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hangxiaodian.Model.JsonDataModel
+{
+    [DataContract]
+    class LoginData
+    {
+        [DataMember]
+        public string mNo { get; set; }
+
+        [DataMember]
+        public string name { get; set; }
+
+        [DataMember]
+        public string department { get; set; }
+
+        [DataMember]
+        public string office { get; set; }
+
+        [DataMember]
+        public string jsessionid { get; set; }
+    }
+}
